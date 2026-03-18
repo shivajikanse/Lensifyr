d9b811962d6d296a81ecbb5372fd5933640767be
shivajikanse <shivajikanse22@gmail.com>
2026-03-18 22:58:17 +0530

    implemented Register and login page with new bg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import RootLayout from "@/components/RootLayout";
-import ProtectedRoute from "@/components/ProtectedRoute";
-import LandingPage from "@/pages/LandingPage";
-import LoginPage from "@/pages/LoginPage";
-import RegisterPage from "@/pages/RegisterPage";
-import DashboardPage from "@/pages/DashboardPage";
+import RootLayout from "./components/RootLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 import EventDetailPage from "./pages/EventDetailPage";
+import FindPhotosPage from "./pages/FindPhotosPage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/find" element={<FindPhotosPage />} />
         <Route
           path="/dashboard"
           element={
